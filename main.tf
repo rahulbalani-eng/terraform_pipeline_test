3105106db9b026e93dd98b41fb66dc48172282f8
rahulbalani-eng <rahul.balani@harness.io>
2026-07-07 16:05:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ terraform {
       }
     }
   } 
-
+  
   provider "aws" {
     region = "us-east-1"
-  }
+  } 
 
   resource "random_id" "suffix" {
     byte_length = 4
-  }
+  } 
 
   resource "aws_security_group" "test_sg" {
     name        = "iacm-state-test-sg-${random_id.suffix.hex}"
@@ -30,6 +30,13 @@ terraform {
       cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  
     egress {
       from_port   = 0
       to_port     = 0
@@ -41,10 +48,10 @@ terraform {
       Name = "iacm-state-test"
     }
   } 
-  
+
   resource "aws_instance" "web" {
-    ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 us-east-1
-    instance_type = "t2.micro"
+    ami                    = "ami-0c02fb55956c7d316"
+    instance_type          = "t2.micro"
     vpc_security_group_ids = [aws_security_group.test_sg.id]
 
     tags = { 
@@ -52,14 +59,30 @@ terraform {
       Env  = "dev"
     }
   } 
-  
+
   resource "aws_instance" "db" {
-    ami           = "ami-0c02fb55956c7d316"
-    instance_type = "t2.micro"
+    ami                    = "ami-0c02fb55956c7d316"
+    instance_type          = "t2.micro"
     vpc_security_group_ids = [aws_security_group.test_sg.id]
   
     tags = { 
       Name = "iacm-state-test-db"
       Env  = "dev"
     }
+  } 
+
+  output "security_group_id" {
+    value = aws_security_group.test_sg.id
+  } 
+  
+  output "web_instance_id" {
+    value = aws_instance.web.id
+  } 
+  
+  output "web_public_ip" {
+    value = aws_instance.web.public_ip
+  }
+  
+  output "db_instance_id" {
+    value = aws_instance.db.id
   }
